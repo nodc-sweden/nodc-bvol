da12d844cecc41fe3d1d0854ff7f46e506521744
Magnus Wenzer <magnus.wenzer@smhi.se>
2025-12-16 17:06:54 +0100

    Add config option and linting

--- src/nodc_bvol/__init__.py
+++ src/nodc_bvol/__init__.py
@@ -7,43 +7,64 @@ from nodc_bvol.bvol_nomp import BvolNomp
 from nodc_bvol.translate_bvol_name import TranslateBvolName
 from nodc_bvol.translate_bvol_name_size import TranslateBvolNameSize
 
+
+def get_user_given_config_dir() -> pathlib.Path | None:
+    path = pathlib.Path(os.getcwd()) / "config_directory.txt"
+    if not path.exists():
+        return
+    with open(path) as fid:
+        config_path = fid.readline().strip()
+        if not config_path:
+            return
+        config_path = pathlib.Path(config_path)
+        if not config_path.exists():
+            return
+        return config_path
+
+
 logger = logging.getLogger(__name__)
 
-CONFIG_ENV = 'NODC_CONFIG'
+CONFIG_ENV = "NODC_CONFIG"
 
 home = pathlib.Path.home()
 OTHER_CONFIG_SOURCES = [
-    home / 'NODC_CONFIG',
-    home / '.NODC_CONFIG',
-    home / 'nodc_config',
-    home / '.nodc_config',
+    home / "NODC_CONFIG",
+    home / ".NODC_CONFIG",
+    home / "nodc_config",
+    home / ".nodc_config",
 ]
 
 CONFIG_FILE_NAMES = [
-    'bvol_nomp.txt',
-    'translate_bvol_name.txt',
-    'translate_bvol_name_size.txt',
+    "bvol_nomp.txt",
+    "translate_bvol_name.txt",
+    "translate_bvol_name_size.txt",
 ]
 
 
 CONFIG_DIRECTORY = None
-if os.getenv(CONFIG_ENV) and pathlib.Path(os.getenv(CONFIG_ENV)).exists():
-    CONFIG_DIRECTORY = pathlib.Path(os.getenv(CONFIG_ENV))
+conf_dir = get_user_given_config_dir()
+if conf_dir:
+    CONFIG_DIRECTORY = conf_dir
 else:
-    for directory in OTHER_CONFIG_SOURCES:
-        if directory.exists():
-            CONFIG_DIRECTORY = directory
-            break
+    if os.getenv(CONFIG_ENV) and pathlib.Path(os.getenv(CONFIG_ENV)).exists():
+        CONFIG_DIRECTORY = pathlib.Path(os.getenv(CONFIG_ENV))
+    else:
+        for directory in OTHER_CONFIG_SOURCES:
+            if directory.exists():
+                CONFIG_DIRECTORY = directory
+                break
 
 
 def get_config_path(name: str = None) -> pathlib.Path:
     if not CONFIG_DIRECTORY:
-        raise NotADirectoryError(f'Config directory not found. Environment path {CONFIG_ENV} does not seem to be set and not other config directory was found. ')
+        raise NotADirectoryError(
+            f"Config directory not found. Environment path {CONFIG_ENV} does not seem to be set and not other config directory was found. "
+        )
     if name not in CONFIG_FILE_NAMES:
         raise FileNotFoundError(f'No config file with name "{name}" exists')
     path = CONFIG_DIRECTORY / name
     if not path.exists():
-        raise FileNotFoundError(f'Could not find config file {name}')
+        raise FileNotFoundError(f"Could not find config file {name}")
     return path
 
 
@@ -65,8 +86,11 @@ def get_bvol_nomp_object() -> "BvolNomp":
     return BvolNomp(path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     tran_name = get_translate_bvol_name_object()
     tran_size = get_translate_bvol_name_size_object()
     nomp = get_bvol_nomp_object()
+    mapper = nomp.get_species_to_aphia_id_mapper()
+    # mapper = nomp.get_carbon_per_volume_mapper()
 
+    _mapper = tran_size.get_scientific_name_from_to_mapper()
--- src/nodc_bvol/bvol_nomp.py
+++ src/nodc_bvol/bvol_nomp.py
@@ -1,26 +1,49 @@
+import functools
 import logging
 import pathlib
 
+import numpy as np
 import polars as pl
 
 logger = logging.getLogger(__name__)
 
 
 class BvolNomp:
-    first_col = 'List'
+    first_col = "List"
 
     def __init__(self, path: str | pathlib.Path):
         self._path = pathlib.Path(path)
-        self._df = None
+        self._df: pl.DataFrame = None
         self._load_file()
         self._cleanup_data()
+        self._add_joined_column()
+
+    @property
+    def path(self) -> pathlib.Path:
+        return self._path
+
+    @property
+    def source(self) -> str:
+        return self.path.name
 
     def _load_file(self) -> None:
-        self._df = pl.read_csv(self._path, separator='\t', encoding='cp1252', infer_schema_length=0)
+        self._df = pl.read_csv(
+            self._path, separator="\t", encoding="cp1252", infer_schema_length=0
+        )
 
     def _cleanup_data(self) -> None:
-        self._df = self._df.filter(~pl.col(self.first_col).str.starts_with('#'))
-        # self._df.columns = [col.lower() for col in self._df.columns]
+        self._df = self._df.filter(~pl.col(self.first_col).str.starts_with("#"))
+
+    def _add_joined_column(self):
+        self._df = self._df.with_columns(
+            pl.concat_str(
+                [
+                    pl.col("Species"),
+                    pl.col("SizeClassNo"),
+                ],
+                separator=":",
+            ).alias("species_and_size_class")
+        )
 
     def get_info(self, **kwargs) -> dict | list | bool:
         """Returns information from nomp list filtered on data in kwargs"""
@@ -31,3 +54,46 @@ class BvolNomp:
         if len(info) == 1:
             return info[0]
         return info
+
+    @functools.cache
+    def get_species_to_aphia_id_mapper(self):
+        df = self._df.filter(pl.col("AphiaID") != "")
+        return dict(zip(df["Species"], df["AphiaID"]))
+
+    @functools.cache
+    def get_species_and_size_class_to_aphia_id_mapper(self):
+        return dict(zip(self._df["species_and_size_class"], self._df["AphiaID"]))
+
+    @functools.cache
+    def get_species_and_size_class_to_ref_list_mapper(self):
+        return dict(zip(self._df["species_and_size_class"], self._df["List"]))
+
+    @functools.cache
+    def get_calculated_volume_mapper(self):
+        mapping = {}
+        for (aphia_id, size_class), df in self._df.group_by(["AphiaID", "SizeClassNo"]):
+            value = df["Calculated_volume_µm3"][0].replace(",", ".").strip()
+            if value:
+                value = float(value)  # * 10e-9
+            else:
+                value = np.nan
+            if aphia_id is None:
+                aphia_id = ""
+            mapping[":".join((aphia_id, size_class))] = value
+        return mapping
+
+    @functools.cache
+    def get_carbon_per_volume_mapper(self):
+        mapping = {}
+        for (aphia_id, size_class), df in self._df.group_by(["AphiaID", "SizeClassNo"]):
+            value = (
+                df["Calculated_Carbon_pg/counting_unit"][0].replace(",", ".").strip()
+            )
+            if value:
+                value = float(value) / 1_000_000
+            else:
+                value = np.nan
+            if aphia_id is None:
+                aphia_id = ""
+            mapping[":".join((aphia_id, size_class))] = value
+        return mapping
